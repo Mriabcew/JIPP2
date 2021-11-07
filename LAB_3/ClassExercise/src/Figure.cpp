21156acfc1a67e00a7f0b2b95e4457b4c6fddfcb
Mriabcew <michal.riabcew@student.pk.edu.pl>
2021-11-07 15:02:38 +0100

    LAB3 _ Classes_v2

---
+++ LAB_3/ClassExercise/src/Figure.cpp
@@ -0,0 +1,9 @@
+#include "../include/Figure.h"
+
+Figure::Figure(int number_of_vertices)
+:number_of_vertices(number_of_vertices)
+{
+    tab=new Point[number_of_vertices];
+    for(int i=0;i<number_of_vertices;i++)
+        tab[i]=Point();
+}
